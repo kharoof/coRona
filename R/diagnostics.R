4fd4a6c6a55dd129d6738395f89f90f8262b61f5
eroche <edderoiste@gmail.com>
2020-04-05 15:13:38 +0100

    Initial Version of Package
    - Gets Confirmed Cases Only

---
+++ R/diagnostics.R
@@ -0,0 +1,80 @@
+#' Fit a Model to the Data
+#'
+#' @param country Country/Region Name
+#' @param model_type Model Type
+#'
+#' @return
+#' @export
+#'
+fit_model <- function(country, model_type="Exponential") {
+
+  #Calculate the duration since the first recorded case in the country
+  country$duration <- country$value.date+1-first(country$value.date)
+
+  if(model_type=="Exponential") {
+    ## i.e. fit y = a*exp(b*x)
+    model <- lm(log(value)~duration, data=country)
+
+    a_value <- exp(model$coefficients[[1]])
+    b_value <- model$coefficients[[2]]
+
+    model <- list(type=model_type,a=a_value, b=b_value)
+  }
+
+  return(model)
+}
+
+
+#' Predict based on fitted model
+#'
+#' Make a prediction based on a fitted model and duration since first case in this country
+#'
+#' @param model Model Object returned from fit_model
+#' @param duration Duration to Predict (from first case in country)
+#'
+#' @return Predictived Value
+#' @export
+#'
+prediction <- function(model, duration) {
+
+  if(model$type=="Exponential") {
+    predicted_value <- model$a * exp(model$b*duration)
+  }
+
+  return(predicted_value)
+}
+
+
+#' Visualise Fitted vs Actual Values
+#'
+#' Graph the trend in numbers and visually compare a fitted curve to the actual data projecting out for 3 days
+#' @param country
+#' @param country_name
+#'
+#' @return
+#' @export
+#'
+visualise_fitted_vs_actual <- function(country, country_name="") {
+
+  if(country_name=="") {
+    country_name <- deparse(substitute(country))
+  }
+
+  fitted_model <- fit_model(country)
+
+
+  #Time period from first case to latest + 3 days
+  #3 days Provides visual indication of actual vs fitted growth
+  time_period <- c(as.Date(country$value.date), last(country$value.date)+1, last(country$value.date)+2, last(country$value.date)+3)
+
+  #Generate the data for plotting
+  fitted <- prediction(fitted_model, 1:(nrow(country)+3))
+  actual <- c(country$value, NA, NA, NA)
+  results <- data.table(value.date = time_period, actual = actual, prediction= fitted)
+
+  ggplot2::ggplot(results, ggplot2::aes(x=value.date))+ggplot2::geom_line(ggplot2::aes(y=actual, color="red"))+
+    ggplot2::geom_point(ggplot2::aes(y=prediction))+ ggplot2::geom_line(ggplot2::aes(y=prediction))+
+    ggplot2::labs( title="Actual vs Expected", subtitle=country_name , x="Date", y="Count")+ ggplot2::theme(legend.position = "")+
+    ggplot2::scale_y_continuous(labels=scales::comma)
+}
+
---
+++ R/get_jh_data.R
@@ -0,0 +1,55 @@
+
+
+#' Download Latest Covid Dataset
+#'
+#' Download the latest dataset from the John Hopkins website
+#'
+#'
+#' @return data.table of confirmed cases by country and date
+#' @export
+#'
+get_confirmed_cases <- function() {
+
+  ##URL of Latest Dataset (Updated Daily)
+  cases_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+
+  confirmed_cases <- data.table::fread(cases_url)
+
+  return(confirmed_cases)
+}
+
+#' Tidy the Raw John Hopkins Data
+#'
+#' Tidy the Raw Data and Add the latest Stats for easy filtering
+#'
+#' @param raw_data
+#'
+#' @return tidy dataset in long format
+#' @export
+#'
+tidy_confirmed_cases <- function(raw_data){
+
+  data <- reshape2::melt(raw_data[,c(-1,-3,-4)], id.var="Country/Region")
+
+  data <- data[, .( value.date = as.Date(variable, "%m/%d/%y"),`Country/Region`,value)]
+  #prune the data so that we only have the data for each country since the first reported case
+  data[, value:=ifelse(value==0, NA, value)]
+  data <- data[!is.na(data$value)]
+
+  ##Data is split out at province/region detail for some countries so we summarise the data by Country. This will be our lower level of granularity for analysis
+  data <- data[, .(value=sum(value)), .(`Country/Region`, value.date)]
+  data.table::setkey(data, 'Country/Region')
+
+  #Stats to enable filtering later
+  stats <- data[,.(first.case=first(value.date), latest.value=last(value)), .(`Country/Region`)]
+  setkey(stats, 'Country/Region')
+
+
+
+  #Append the Stats to the cleaned John Hopkins Data
+  confirmed_cases <- stats[data]
+
+  return(confirmed_cases)
+}
+
+
